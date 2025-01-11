eeed498c6e712ed220b49cbdec10788856c42401
Karan Lodaya <148992626+KaranLodaya@users.noreply.github.com>
2025-01-11 15:54:40 +0530

    Update settings.py

--- task_managment/settings.py
+++ task_managment/settings.py
@@ -154,5 +154,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='karanlodaya363@gmail.com'
-EMAIL_HOST_PASSWORD ='objj ldyx jgvw gxnh'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD = ''
+
